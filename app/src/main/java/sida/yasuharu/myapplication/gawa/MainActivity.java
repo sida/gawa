62ac65c5fb9565860ca8fea61ba3addcaa9bd7d3
信太　康治 <shida@antepost.jp>
2022-05-09 17:54:55 +0900

    Backボタンに対応

--- app/src/main/java/sida/yasuharu/myapplication/gawa/MainActivity.java
+++ app/src/main/java/sida/yasuharu/myapplication/gawa/MainActivity.java
@@ -9,15 +9,24 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView webView;
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = findViewById(R.id.wvContent);
+        webView = findViewById(R.id.wvContent);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://google.co.jp");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView==null) { return;}
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
     }
 }
