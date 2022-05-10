1014fb056195aac73fb687ec04892891bd8e4781
信太　康治 <shida@antepost.jp>
2022-05-10 11:56:19 +0900

    URL入力を実装

--- app/src/main/java/sida/yasuharu/myapplication/gawa/MainActivity.java
+++ app/src/main/java/sida/yasuharu/myapplication/gawa/MainActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import sida.yasuharu.myapplication.gawa.Util.MyUtil;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     boolean waitingClose = false;
+    String homeUrl = "https://google.co.jp";
 
     WebView webView;
     @SuppressLint("SetJavaScriptEnabled")
@@ -19,10 +24,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        (findViewById(R.id.btSetting)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+
         webView = findViewById(R.id.wvContent);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://google.co.jp");
+        webView.loadUrl(homeUrl);
     }
 
     @Override
@@ -46,5 +58,27 @@ public class MainActivity extends AppCompatActivity {
                 waitingClose = false;
             }
         });
+    }
+
+    private void openDialog() {
+        final EditText editText = new EditText(this);
+        editText.setHint("https://example.co.jp");
+        editText.setText(webView.getUrl());
+        new AlertDialog.Builder(this)
+                .setView(editText)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        homeUrl = editText.getText().toString();
+                        webView.loadUrl(homeUrl);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .show();
     }
 }
